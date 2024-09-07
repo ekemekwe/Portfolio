800f4b754cb1aae8887859c2588a4b827ceace76
ekemekwe <46725424+ekemekwe@users.noreply.github.com>
2024-09-07 03:42:49 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -102,7 +102,7 @@ function Home2() {
             //       <AiFillInstagram />
             //     </a>
             //   </li>
-            </ul>
+            // </ul>
           </Col>
         </Row>
       </Container>
